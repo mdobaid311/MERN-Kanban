c676c24a01d521e594ae9b5f6f6c287d4cb3abe2
mdobaid311 <mdobaid311@gmail.com>
2022-08-17 00:55:46 +0530

    initial commit

--- server/src/v1/handlers/validation.js
+++ server/src/v1/handlers/validation.js
@@ -7,7 +7,7 @@ exports.validate = (req, res, next) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         return res.status(400).json({
-            errors: error.array()
+            errors: errors.array()
         })
     }
     next()
